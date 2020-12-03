220f53515de5add1fa614a4933db05e1d2752c1b
1093357043 <73806828+1093357043@users.noreply.github.com>
2020-12-03 15:29:25 +0800

    .

--- src/main/java/com/alipay/openapi/AlipayAPI.java
+++ src/main/java/com/alipay/openapi/AlipayAPI.java
@@ -1,6 +1,7 @@
 package com.alipay.openapi;
 
 
+import com.alipay.api.response.*;
 import com.alipay.openapi.entity.*;
 
 public abstract class AlipayAPI {
@@ -16,15 +17,15 @@ public abstract class AlipayAPI {
             String app_auth_token) {
 
     }
-    public abstract alipay_system_oauth_token_response alipay_system_oauth_token(String grant_type, String code, String refresh_token) throws AlipayError;
+    public abstract AlipaySystemOauthTokenResponse alipay_system_oauth_token(String grant_type, String code, String refresh_token) throws AlipayError;
 
-    public abstract alipay_open_app_qrcode_create_response alipay_open_app_qrcode_create(String url_param, String query_param, String describe) throws AlipayError;
+    public abstract AlipayOpenAppQrcodeCreateResponse alipay_open_app_qrcode_create(String url_param, String query_param, String describe) throws AlipayError;
 
-    public abstract alipay_merchant_item_file_upload_response alipay_merchant_item_file_upload(String scene, byte[] file_content) throws AlipayError;
+    public abstract AlipayMerchantItemFileUploadResponse alipay_merchant_item_file_upload(String scene, byte[] file_content) throws AlipayError;
 
-    public abstract alipay_open_mini_content_sync_response alipay_open_mini_content_sync(alipay_open_mini_content_sync_body body) throws AlipayError;
+    public abstract AlipayOpenAppMiniTemplatemessageSendResponse alipay_open_mini_content_sync(alipay_open_mini_content_sync_body body) throws AlipayError;
 
-    public abstract ant_merchant_expand_shop_query_response ant_merchant_expand_shop_query(ant_merchant_expand_shop_query_body body) throws AlipayError;
+    public abstract AntMerchantExpandShopQueryResponse ant_merchant_expand_shop_query(ant_merchant_expand_shop_query_body body) throws AlipayError;
 
-    public abstract monitor_heartbeat_syn_response monitor_heartbeat_syn(String biz_content) throws AlipayError;
+    public abstract MonitorHeartbeatSynResponse monitor_heartbeat_syn(String biz_content) throws AlipayError;
 }
